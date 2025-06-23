ec1276bd42d2d7cfbd1d357a035e32b910a23cdb
Muzamil Zaman <95095602+MuzamilDev@users.noreply.github.com>
2025-06-23 01:19:43 -0700

    Create FindLargest.py

---
+++ FindLargest.py
@@ -0,0 +1,6 @@
+num_1 = float(input("Enter first number"))
+num_2 = float(input("Enter Second number"))
+num_3 = float(input("Enter third number"))
+
+largest = max(num_1,num_2,num_3)  #built-in function maximum 
+print(f"Largest number is: {largest}")
